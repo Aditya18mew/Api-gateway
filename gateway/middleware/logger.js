ea1a32e5e86533a0d4b8ff59b737929a6bf28379
Aditya <parmaraditya242@gmail.com>
2026-05-24 17:06:38 +0530

    improved logging

--- gateway/middleware/logger.js
+++ gateway/middleware/logger.js
@@ -1,17 +1,18 @@
+const pino=require("pino")
 const { v4: uuidv4 } = require("uuid")
-
+const log=pino()
 const logger=(req,res,next)=>{
 
     const requestId=req.headers["x-request-id"] || uuidv4()
       req.id=requestId
+      res.setHeader("x-request-id",requestId)
+      req.headers["x-request-id"]=requestId
    
   const start=Date.now()
 
   res.on("finish",()=>{
     const end=Date.now()
-     console.log(
-         `${req.method} ${req.url} ${res.statusCode} ${req.id} ${end-start}ms`
-      )
+    log.info({requestId,method:req.method,url:req.url, status:res.statusCode, duration:`${end-start}ms`})
   })
   next();
 }
